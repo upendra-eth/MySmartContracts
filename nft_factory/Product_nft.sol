63baca518a7daf8a5c64e3f7fe3745a8ba04e4a9
UPENDRA SINGH <102982552+UV4Crypto@users.noreply.github.com>
2022-08-11 18:50:25 +0530

    changes

---
+++ nft_factory/Product_nft.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract profile_nft is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor(string memory _nft_name,string memory _symbol) ERC721(_nft_name, _symbol) {}
+
+    function awardItem(address player, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
